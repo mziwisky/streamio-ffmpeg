5fde980881b7988767d12c7a2c9dd637d5ae02f6
Michael Ziwisky <mziwisky@instructure.com>
2016-04-14 16:30:14 -0500

    expose Media#ffprobe_stderr
    this is a nice value for a consumer to log if there ends up being a
    problem with the Media

--- lib/ffmpeg/movie.rb
+++ lib/ffmpeg/movie.rb
@@ -3,7 +3,7 @@ require 'multi_json'
 
 module FFMPEG
   class Movie
-    attr_reader :path, :duration, :time, :bitrate, :rotation, :creation_time, :raw_metadata
+    attr_reader :path, :duration, :time, :bitrate, :rotation, :creation_time, :raw_metadata, :ffprobe_stderr
     attr_reader :video_stream, :video_codec, :video_bitrate, :colorspace, :width, :height, :sar, :dar, :frame_rate
     attr_reader :audio_stream, :audio_codec, :audio_bitrate, :audio_sample_rate, :audio_channels
     attr_reader :container
@@ -29,6 +29,7 @@ module FFMPEG
       fix_encoding(std_error)
 
       @raw_metadata = MultiJson.load(std_output, symbolize_keys: true)
+      @ffprobe_stderr = std_error
 
       if raw_metadata.key?(:error)
 
